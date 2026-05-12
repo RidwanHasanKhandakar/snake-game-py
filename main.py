154e2ea430e0c6639a6570bd6666c25e90b63eb7
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-12 19:24:22 +0600

    made all the class & functions and imported tkinter

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import*
+import random
+class snake:
+    pass
+class food:
+    pass
+def next_tuen():
+    pass
+def change_dir(new_dir):
+    pass
+def check_coll():
+    pass
+def game_over():
+    pass
