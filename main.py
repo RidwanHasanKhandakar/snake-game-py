104bed05d75f1643e1f4c93ffd56fa157c1407bc
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-12 20:07:18 +0600

    added food

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ BODY_PARTS=3
 SNAKE_COLOR="#00ff00"
 FOOD_COLOR="#ff0000"
 BACKGROUND_COLOR="#000000"
-class snake:
+class Snake:
     pass
-class food:
-    pass
+class Food:
+    def __init__(self):
+        x=random.randint(0,(GAME_WIDTH//SPACE_SIZE)-1)*SPACE_SIZE
+        y=random.randint(0,(GAME_HEIGHT//SPACE_SIZE)-1)*SPACE_SIZE
+        self.coordinates=[x,y]
+        canvas.create_oval(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=FOOD_COLOR,tag="food")
 def next_tuen():
     pass
 def change_dir(new_dir):
@@ -25,7 +29,7 @@ window.title("Snake game")
 window.resizable(False,False)
 scr=0
 dir='down'
-label=Label(window,text="SCORE{}".format(scr),font=("impact",40))
+label=Label(window,text="SCORE {}".format(scr),font=("impact",40))
 label.pack()
 canvas=Canvas(window,bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
 canvas.pack()
@@ -34,4 +38,9 @@ window_width=window.winfo_width()
 window_height=window.winfo_height()
 screen_width=window.winfo_screenwidth()
 screen_height=window.winfo_screenheight()
+x=int((screen_width/2)-(window_width/2))
+y=int((screen_height/2)-(window_height/2))
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+snake=Snake()
+food=Food()
 window.mainloop()
